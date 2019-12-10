7249acd450ab5cb531781d37da3aa6400d3d9959
ayush2297 <ayush.saraf47@gmail.com>
2019-12-10 14:06:36 +0530

    added: IndiaCensusDAO to support DAO pattern implementation

---
+++ src/main/java/censusanalyser/IndiaCensusDAO.java
@@ -0,0 +1,16 @@
+package censusanalyser;
+
+public class IndiaCensusDAO {
+
+    public String state;
+    public int areaInSqKm;
+    public int densityPerSqKm;
+    public int population;
+
+    public IndiaCensusDAO(IndiaCensusCSV indiaCensusCsv) {
+        state = indiaCensusCsv.state;
+        areaInSqKm = indiaCensusCsv.areaInSqKm;
+        densityPerSqKm = indiaCensusCsv.densityPerSqKm;
+        population = indiaCensusCsv.population;
+    }
+}
